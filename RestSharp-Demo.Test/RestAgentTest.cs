68684293afb0e35ef3b65f3394303e762791d9bc
Bryan Wood <itanex@gmail.com>
2018-03-12 22:13:13 -0700

    Add Code Solution

---
+++ RestSharp-Demo.Test/RestAgentTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace RestSharp_Demo.Test
+{
+    [TestClass]
+    public class RestAgentTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var agent = new RestAgent();
+            var testObject = new { key1 = "value1", key2 = "value2" };
+            var testParameter = new Parameter()
+            {
+                Name = "application/json",
+                Type= ParameterType.RequestBody,
+                Value = "{\"key1\":\"value1\",\"key2\":\"value2\"}"
+            };
+            IRestRequest actual = agent.HttpPost(testObject);
+
+            // in case that the casted object fails
+            Assert.IsNotNull(actual);
+
+            Assert.IsTrue(actual.Method == Method.POST);
+            Assert.IsTrue(actual.Parameters.Count == 2);
+
+            Assert.IsTrue(ContainsParameter(actual.Parameters.Where(x => x.Type.Equals(ParameterType.RequestBody)).DefaultIfEmpty(), testParameter));
+        }
+
+        private bool ContainsParameter(IEnumerable<Parameter> parameters, Parameter value)
+        {
+            foreach (var param in parameters)
+            {
+                var paramValue = param.Value.ToString();
+                var testValue = value.Value.ToString();
+
+                if (paramValue.Equals(testValue) && param.Name.Equals("application/json"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ RestSharp-Demo/RestAgent.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Web.Script.Serialization;
+using RestSharp;
+
+namespace RestSharp_Demo
+{
+    public class RestAgent
+    {
+        public IRestRequest HttpPost(object PostContent)
+        {
+            var client = new RestClient("http://localhost");
+            var request = new RestRequest();
+
+            var strJSONContent = new StringBuilder();
+
+            (new JavaScriptSerializer()).Serialize(PostContent, strJSONContent);
+
+            request.Method = Method.POST;
+            request.AddHeader("Accept", "application/json");
+            request.Parameters.Clear();
+            request.AddParameter("application/json", strJSONContent, ParameterType.RequestBody);
+
+            client.Execute(request);
+
+            return request;
+        }
+    }
+}
